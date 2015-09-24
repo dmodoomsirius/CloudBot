1e897d15ff567448ccc00217fd58179bd13c06c4
dmodoomsirius <dmodoomsirius@gmail.com>
2015-09-23 23:00:23 -0400

    merge bbmbot into BBM-Bot

---
+++ plugins/bbm.py
@@ -0,0 +1,37 @@
+from cloudbot import hook
+
+bbmChannels = ["#bbm-bots","#bbm-dev","#builtbroken","#builtbrokenmodding","#bbm-packs","#icbm","#artillects "]
+
+@hook.command("bbmstaff")
+def bbmStaff(text, message, chan):
+    if any(x in chan for x in bbmChannels):
+            message("Owners: Dmodoomsirius, DarkGuardsman");
+            #message("Texture Artist: Morton0000");
+            message("Senior Developers: Kolatra")
+            message("Junior Developers: TheCowGod, Hennamann")
+    else:
+            message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+
+
+
+@hook.command("sponsor")
+def sponsor(text, message, chan):
+    if any(x in chan for x in bbmChannels):
+            message("BuildBrokenModding servers both Beta test servers and build server");
+            message("is sponsored by Akliz.");
+            message("http://www.akliz.net/bbm")
+    else:
+            message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+            
+@hook.command("bbmchan")
+def chans(text, message):
+    message("The official BuiltBroken Channels are: " + " , ".join(bbmChannels))
+    
+@hook.command("bbmhelp")
+def bbmhelp(text, message, bot):
+    if any(x in chan for x in bbmChannels):
+            message("If you are looking for who is the staff type " + bot.config["connections"][0]["command_prefix"] + "bbmstaff")
+            message ("if you are looking for our sponsors please type " + bot.config["connections"][0]["command_prefix"] + "sponsor")
+            message("If you are looking for our official channels please do  " + bot.config["connections"][0]["command_prefix"] + "bbmchan")
+    else:
+            message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
---
+++ plugins/bbm_packs.py
@@ -0,0 +1,35 @@
+from cloudbot import hook
+
+bbmChannels = ["#bbm-bots","#bbm-dev","#builtbroken","#builtbrokenmodding","#bbm-packs"]
+
+@hook.command("voltz")
+def bbmVoltz(text, message, chan):
+    if any(x in chan for x in bbmChannels):
+            message("Voltz for minecraft version 1.7.10 is in beta");
+            message ("we are hoping to have a beta out sometime next soonTM");
+    else:
+            message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+
+@hook.command("Ampz", "Ampzreloaded")
+def bbmAmpzReloaded(text, message, chan):
+    if any(x in chan for x in bbmChannels):
+                message("Ampz-reloaded is the new imporoved Ampz.")
+                message("It is currently on hold.")
+                return
+    message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+
+@hook.command("omhz")
+def omhz(text, message, chan):
+    if any(x in chan for x in bbmChannels):
+                message("currently on hold until Voltz is done.")
+                return
+    message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+    
+@hook.command("packs")
+def bbmpacks(text, message, chan, bot):
+    if any(x in chan for x in bbmChannels):
+                message("For Voltz type " + bot.config["connections"][0]["command_prefix"] + "voltz")
+                message("For AmpzReloaded info type either " + bot.config["connections"][0]["command_prefix"] + "Ampz or " + bot.config["connections"][0]["command_prefix"] + "Ampzreloaded")
+                message("For omhz type  " + bot.config["connections"][0]["command_prefix"] + "omhz")
+                return
+    message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
