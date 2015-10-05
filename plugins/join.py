6f96ed5ad60f735e3314c6a75bed7c46827ab0ee
Dmodoom Sirius <dmodoomsirius@users.noreply.github.com>
2015-10-05 16:57:50 -0400

    Update join.py

--- plugins/join.py
+++ plugins/join.py
@@ -1,4 +1,3 @@
-
 from cloudbot import hook
 
 @hook.irc_raw('004')
@@ -9,3 +8,8 @@ def onReadyJoin(conn, bot):
     conn.send("JOIN #BuiltBroken")
     #conn.send("JOIN #Razz")
     conn.send("JOIN #MadScience")
+    conn.send("JOIN #icbm")
+    conn.send("JOIN #dmod")
+    conn.send("JOIN #Artillects")
+    conn.send("JOIN #Kolatra")
+    conn.send("JOIN #Kolatra-Dev")
