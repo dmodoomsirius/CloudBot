addfe6356358652045b09e1b38b85294d8c36c7f
Tyler J. L. <Kolatra@users.noreply.github.com>
2015-10-09 18:51:31 -0600

    Update bbm_packs.py

--- plugins/bbm_packs.py
+++ plugins/bbm_packs.py
@@ -2,13 +2,13 @@ from cloudbot import hook
 
 bbmChannels = ["#bbm-bots","#bbm-dev","#builtbroken","#builtbrokenmodding","#bbm-packs","#icbm","#artillects "]
 
-@hook.command("voltz")
-def bbmVoltz(text, message, chan):
-    if any(x in chan for x in bbmChannels):
-            message("Voltz for minecraft version 1.7.10 is in beta");
-            message ("we are hoping to have a beta out sometime next soonTM");
-    else:
-            message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
+# @hook.command("voltz")
+#def bbmVoltz(text, message, chan):
+ #   if any(x in chan for x in bbmChannels):
+  #          message("Voltz for minecraft version 1.7.10 is in beta");
+   #         message ("we are hoping to have a beta out sometime next soonTM");
+    #else:
+     #       message("Command can only be run in Official BBM Channels. Join #bbm-bots to run the command.")
 
 @hook.command("Ampz", "Ampzreloaded")
 def bbmAmpzReloaded(text, message, chan):
